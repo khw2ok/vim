4c08f3a9fb4e9ca6bbb1c7933097980879fd495b
김형욱 <idkimwook@gmail.com>
2025-02-19 15:53:54 +0900

    Change colorschemes

--- lua/khw2/plugins/256k.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "https://github.com/khw2ok/vim-256k",
-  config = function()
-    vim.cmd.colorscheme "256k"
-    --vim.cmd "highlight Pmenu ctermbg=black guibg=black"
-  end
-}
--- lua/khw2/plugins/outline.lua
+++ lua/khw2/plugins/outline.lua
@@ -2,6 +2,8 @@ return {
   "hedyhli/outline.nvim",
   config = function ()
     vim.keymap.set("n", "<leader>o", "<cmd>Outline<CR>", { desc = "Toggle Outline" })
-    require("outline").setup()
+    require("outline").setup({
+      width = 20
+    })
   end
 }
